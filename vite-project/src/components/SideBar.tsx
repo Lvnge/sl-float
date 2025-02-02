bd3d006fa3ea5c59ff7d1fdc05302b564fef9520
Lvnge <roy9096@hotmail.com>
2025-02-02 09:14:51 -0600

    Sidebar style improved

--- vite-project/src/components/SideBar.tsx
+++ vite-project/src/components/SideBar.tsx
@@ -142,10 +142,12 @@ const Sidebar = () => {
             </svg>
           </button>
           {isMoreOpen && (
-            <div className=" flex flex-col mt-2 space-y-2 bg-gray-50 dark:bg-gray-800 rounded-lg shadow-lg">
+            <div className=" flex flex-col mt-2  bg-gray-50 dark:bg-gray-800 rounded-lg shadow-lg">
               <Link
                 to="/transactions"
-                className="flex items-center text-gray-600 hover:text-indigo-700 dark:text-white dark:hover:hover:text-indigo-500 py-2 px-4 rounded-lg  "
+                className="flex items-center
+                justify-start text-gray-600
+                hover:bg-gray-600 dark:text-white py-4 px-4 rounded-lg  "
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -163,7 +165,9 @@ const Sidebar = () => {
               </Link>
               <Link
                 to="/categories"
-                className="flex items-center text-gray-600 hover:text-indigo-700 dark:text-white dark:hover:text-indigo-500 py-2 px-4 rounded-lg "
+                className="flex items-center
+                justify-start text-gray-600
+                hover:bg-gray-600 dark:text-white py-4 px-4 rounded-lg "
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -177,7 +181,7 @@ const Sidebar = () => {
                     d="M5.925 21q-.575 0-1.112-.4t-.713-.975q-.625-2.1-1.025-3.637t-.638-2.7t-.337-2.063T2 9.5q0-2.3 1.6-3.9T7.5 4h5q.675-.9 1.713-1.45T16.5 2q.625 0 1.063.438T18 3.5q0 .15-.038.3t-.087.275q-.1.275-.187.55t-.138.6L19.825 7.5H21q.425 0 .713.288T22 8.5v5.25q0 .325-.187.575t-.513.375l-2.125.7l-1.25 4.175q-.2.65-.725 1.038T16 21h-2q-.825 0-1.412-.587T12 19h-2q0 .825-.587 1.413T8 21zM16 11q.425 0 .713-.288T17 10t-.288-.712T16 9t-.712.288T15 10t.288.713T16 11m-4-2q.425 0 .713-.288T13 8t-.288-.712T12 7H9q-.425 0-.712.288T8 8t.288.713T9 9z"
                   />
                 </svg>
-                Categories & Budget
+                Categories
               </Link>
             </div>
           )}
